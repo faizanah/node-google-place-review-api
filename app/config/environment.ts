a7b34593d5b2a71816593c310c8f905bacbfcf89
Faizan AH <faizan.ahmad@virtualforce.io>
2018-12-03 10:34:39 +0100

    set mailer environment variables

--- app/config/environment.ts
+++ app/config/environment.ts
@@ -3,7 +3,16 @@ module.exports = {
    development: {
      DATABASE_URL:  process.env.DATABASE_URL,
      secret:  process.env.SECRET,
-     port: 3000
+     port: 3000,
+     mailer: {
+       from: '"IMFO" <no-reply@imfo.com>',
+       service: 'SendGrid',
+       smtp: {
+         user: 'Your Sendgrid Username',
+         password: 'Your Sendgrid Password'
+       }
+     },
+     host: 'http://localhost:3000'
    },
    test: {
      DATABASE_URL: 'Test DB Connection String',
@@ -13,6 +22,15 @@ module.exports = {
    production: {
      DATABASE_URL: process.env.DATABASE_URL,
      secret: process.env.SECRET,
-     port: process.env.PORT || 3000
+     port: process.env.PORT || 3000,
+     host: process.env.HOST,
+     mailer: {
+       from: process.env.MAILER_FROM,
+       service: process.env.MAILER_SERVICE || 'SendGrid',
+       smtp: {
+         user: process.env.MAILER_USER,
+         password: process.env.MAILER_PASSWORD
+       }
+     }
    }
  }
