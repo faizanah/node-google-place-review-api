f486532aff3f2dccfb781f4ff112fc2a0a901e6c
Faizan AH <faizan.ahmad@virtualforce.io>
2018-12-03 10:32:58 +0100

    set DATABASE Connection string

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL'
+    url: process.env.DATABASE_URL
   },
   test: {
-    use_env_variable: 'DATABASE_URL'
+    url: process.env.DATABASE_URL
   },
   production: {
-    use_env_variable: 'DATABASE_URL'
+    url: process.env.DATABASE_URL
   }
 }
