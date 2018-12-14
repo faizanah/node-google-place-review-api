22cc8230b59165e4da69836c203517c255f8e285
Faizan AH <faizan.ahmad@virtualforce.io>
2018-12-14 13:36:40 +0100

    change user signup response

--- app/controllers/registration.controller.ts
+++ app/controllers/registration.controller.ts
@@ -1,13 +1,21 @@
-import { ApplicationController } from './'
-export class RegistrationController extends ApplicationController {
+let params = {body: {}, condition: {}, pick: ['email', 'fullName', 'password' ]}
+
+export class RegistrationController {
   constructor() {
-    super('User')
   }
   signup(req, res) {
     req.checkBody('email', 'Enter a valid email address.').isEmail().isLength({ min: 3 , max: 100 })
     req.checkBody('fullName', 'Full Name must be between 2 and 50 characters in length.').isLength({ min: 2 , max: 50 })
     req.checkBody('password', 'Password should be at least 6 chars long.').isLength({ min: 6 })
-    req.pick = ['email', 'fullName', 'password' ]
-    return super._create(req , res, {message: 'Congrats! You have successfully registered'} )
+    req.model('User').create(params, data => {
+      const token = data.generateToken()
+      res.setHeader('x-access-token', token)
+      return res.status(201).send({
+        success: true,
+        data: data,
+        token: token,
+        message: 'Congrats! You have successfully registered'
+      })
+    })
   }
 }
