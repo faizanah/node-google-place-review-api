e4b7512ff2e766d897f571c48047d041447c2b6a
Faizan AH <faizan.ahmad@virtualforce.io>
2019-01-08 12:06:01 +0100

    change places order by desc

--- app/controllers/places.controller.ts
+++ app/controllers/places.controller.ts
@@ -1,24 +1,24 @@
-let params = {body: {}, condition: {}, pick: {}}
+let params = { body: {}, condition: {}, pick: {} }
 export class PlacesController {
-  constructor() {}
+  constructor() { }
   create(req, res) {
-    params.condition = {where: {googlePlaceId: req.body.googlePlaceId}, defaults: {}}
+    params.condition = { where: { googlePlaceId: req.body.googlePlaceId }, defaults: {} }
     req.model('Place').findOrCreate(params)
   }
   show(req, res) {
-    params.condition = {where: {'$or': [{id: req.params.id}, {googlePlaceId: req.params.id}]}, include: [ {model: req.db['Review'], as: 'reviews', include: ['createdBy', 'attachments']} ]}
+    params.condition = { where: { '$or': [{ id: req.params.id }, { googlePlaceId: req.params.id }] }, include: [{ model: req.db['Review'], as: 'reviews', include: ['createdBy', 'attachments'] }] }
     return req.model('Place').findOne(params)
   }
   list(req, res) {
     params.condition = {}
-    if (typeof(req.query['search']) !== 'undefined' && req.query['search'] !== '')
-      params.condition = { where: {name: {$like: '%' + req.query.search + '%'} }}
-    if (typeof(req.query['topSort']) !== 'undefined' && req.query['topSort'] !== '')
-      params.condition['order'] = [ ['reviewsCount', req.query['topSort'] ]]
-    if (typeof(params.condition['order']) !== 'undefined')
-      params.condition['order'].push(['createdAt', 'ASC'])
+    if (typeof (req.query['search']) !== 'undefined' && req.query['search'] !== '')
+      params.condition = { where: { name: { $like: '%' + req.query.search + '%' } } }
+    if (typeof (req.query['topSort']) !== 'undefined' && req.query['topSort'] !== '')
+      params.condition['order'] = [['reviewsCount', req.query['topSort']]]
+    if (typeof (params.condition['order']) !== 'undefined')
+      params.condition['order'].push(['createdAt', 'DESC'])
     else
-      params.condition['order'] = [['createdAt', 'ASC']]
+      params.condition['order'] = [['createdAt', 'DESC']]
     return req.model('Place').findAll(params)
   }
 }
