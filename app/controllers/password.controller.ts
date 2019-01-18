ea71236ab0a70f585341138486ff0e010d0e69da
Faizan AH <faizan.ahmad@virtualforce.io>
2019-01-18 10:46:24 +0500

    added new endpoint to change password

--- app/controllers/password.controller.ts
+++ app/controllers/password.controller.ts
@@ -1,4 +1,4 @@
-let params = {body: {}, condition: {}, pick: {}}
+let params = { body: {}, condition: {}, pick: ['password'] }
 export class PasswordController {
   constructor() {
   }
@@ -8,9 +8,19 @@ export class PasswordController {
     return req.model('User').findOne(params, (user) => {
       if (user) {
         user.sendResetPasswordInstructions()
-        return res.status(200).json({success: true, message: 'Successfully! sent password instructions.'})
+        return res.status(200).json({ success: true, message: 'Successfully! sent password instructions.' })
       } else
-        res.boom.notFound({ success: false, errors: [{ message: 'No account with that email address exists.'  }] })
+        res.boom.notFound({ success: false, errors: [{ message: 'No account with that email address exists.' }] })
     })
+  }
+  update(req, res) {
+    req.checkBody('newPassword', 'New password should be at least 6 chars long.').isLength({ min: 6 })
+    req.checkBody('confirmPassword', 'Confirm password should be at least 6 chars long.').isLength({ min: 6 })
+    if (req.body.newPassword === req.body.confirmPassword) {
+      req.body.password = req.body.newPassword
+      params.condition = { where: { id: req.user.id } }
+      return req.model('User').updateOne(params)
+    } else
+      res.unprocessableEntity('New Password and confirm password does not match.')
   }
 }
